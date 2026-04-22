75c896ac52fec916309f355a5beeef8bca30461b
GDA86 <grushinda86@gmail.com>
2026-04-22 20:06:18 +0300

    v3

--- models/model_trian.py
+++ models/model_trian.py
@@ -10,12 +10,37 @@ from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.datasets import load_diabetes
 from sklearn.feature_selection import SelectKBest, f_regression
 
+from sklearn.metrics import classification_report
+
 from sklearn.ensemble import GradientBoostingClassifier
 
-#--------------------------------------------Featur_Engineering--------------------------------------------#
-class My_Featur_Engineering(BaseEstimator, TransformerMixin):
-    def __init__():
 
+import tkinter as tk
+from tkinter import messagebox
+
+#--------------------------------------------Featur_Engineering--------------------------------------------#
+
+
+
+def choose_mode():
+    root = tk.Tk()
+    root.withdraw() # Скрываем основное маленькое окно
+    
+    # Создаем окно с вопросом
+    # Возвращает True (Да) или False (Нет)
+    result = messagebox.askyesnocancel(
+        "Вывести отчет по обучению?", 
+        "Нажмите 'Да': Обучение и Отчет \n"
+        "Нажмите 'Нет': Обучение и Сохранение (Dump)\n"
+        "Нажмите 'Отмена' для выхода"
+    )
+    
+    root.destroy()
+    return result
+
+
+class My_Featur_Engineering(BaseEstimator, TransformerMixin):
+    def __init__(self):
         pass 
     
     def fit(self, X, y=None):
@@ -54,13 +79,13 @@ class My_Featur_Engineering(BaseEstimator, TransformerMixin):
 
         # Считаем количество месяцев с просрочкой
         X['months_with_delay'] = (X[pay_status_cols] > 0).sum(axis=1)
-
+        
+        X = X.fillna(0)
+        X = X.replace([np.inf, -np.inf], 0).fillna(0)
 
         X = X.drop(columns=['ID'])
 
         return X
-
-
 
 def load_mmodel(name):
     # Десериализуем pipeline из файла
@@ -68,26 +93,76 @@ def load_mmodel(name):
         loaded_pipe = pickle.load(pkl_file)
 
 
+def dump_model(name, model):
+    # Сериализуем pipeline и записываем результат в файл
+    with open(name, 'wb') as output:
+        pickle.dump(model, output)
+
+        
+
+def train_model(X, y):
+    
+    lo_pipe = Pipeline([  
+    ('FeatureEngineering', My_Featur_Engineering()),
+    ('FeatureSelection', SelectKBest(f_regression, k=5)),
+    ('GradientBoostingClassifier', GradientBoostingClassifier(n_estimators=100, learning_rate=0.1, max_depth=3,random_state=42))
+    ])
+
+    # Обучаем пайплайн
+    lo_pipe.fit(X, y)
+    
+    return lo_pipe
+
+
+def my_classification_report(model, X, y):
+    predictions = model.predict(X)
+    print("Отчет по классификации:")
+    print(classification_report(y, predictions)) 
+
+
+def read_df(name='models/UCI_Credit_Card.csv'):
+    return pd.read_csv(name)
+
+
 #--------------------------------------------Fit model --------------------------------------------#
-go_train = pd.read_csv('UCI_Credit_Card.csv')
+def main_with_report():
+    go_train = read_df()
 
-pipe = Pipeline([  
-  ('FeatureEngineering', My_Featur_Engineering()),
-  ('FeatureSelection', SelectKBest(f_regression, k=5)),
-  ('GradientBoostingClassifier', GradientBoostingClassifier(n_estimators=100, learning_rate=0.1, max_depth=3,random_state=42))
-  ])
+    X = go_train.drop(columns='default.payment.next.month')
+    y = go_train['default.payment.next.month']
+
+    go_pipe = train_model(X, y)
+
+    my_classification_report(go_pipe, X, y)
 
 
-X = go_train.drop(columns='default.payment.next.month')
-y = go_train['default.payment.next.month']
+def main_with_dump_model():
+    go_train = read_df()
 
-# Обучаем пайплайн
-pipe.fit(X, y)
+    X = go_train.drop(columns='default.payment.next.month')
+    y = go_train['default.payment.next.month']
 
-name = 'model_v1.pkl'
-# Сериализуем pipeline и записываем результат в файл
-with open(name, 'wb') as output:
-    pickle.dump(pipe, output)
+    gv_name = 'models/model_v1.pkl'
+
+    go_pipe = train_model(X, y)
+
+    dump_model(gv_name,go_pipe)
+    
+
+if __name__ == "__main__":
+    mode = choose_mode()
+    
+    if mode is None: #  Отмена
+        print("Действие отменено пользователем.")
+
+    if mode is True:
+        main_with_report()
+
+    if mode is False:
+        main_with_dump_model()
+
+
+
 
 
 
